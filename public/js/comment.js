ae362bd0c92c1f2742633b72c3afac50ef1eec5e
Heather Noyes <heatherknoyes@gmail.com>
2022-06-09 18:12:01 -0400

    Added front end js for handling deleting and updating comments on posts

--- public/js/comment.js
+++ public/js/comment.js
@@ -16,6 +16,42 @@ const newCommentHandler = async (evnt) => {
   document.location.replace(`/blogs/${id}`);
 };
 
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    await fetch(`/api/comments/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    document.location.replace(`/blogs/${id}`);
+  }
+};
+
+const newUpdateHandler = async (evnt) => {
+  evnt.preventDefault();
+  // const title = document.querySelector('.card-header').innerHTML;
+  const content = document.querySelector('#edit-comment-textarea').value;
+  const id = evnt.target.getAttribute('data-id');
+  await fetch(`/api/comments/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      content,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  document.location.replace('/dashboard');
+};
+
 document
   .querySelector('#comment-submit')
   .addEventListener('click', newCommentHandler);
+
+document
+  .querySelector('#delete-comment-button')
+  .addEventListener('click', delButtonHandler);
+
+document
+  .querySelector('#edit-blog-save-button')
+  .addEventListener('click', newUpdateHandler);
