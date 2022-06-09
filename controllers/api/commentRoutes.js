c1aea4b68c88c9628e7fbc6d80d4d4ed5cfd95f9
Heather Noyes <heatherknoyes@gmail.com>
2022-06-09 18:08:29 -0400

    Added backend routes for comment deletion and update

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -7,12 +7,48 @@ router.post('/', withAuth, async (req, res) => {
     const newComment = await Comment.create({
       ...req.body,
       userId: req.session.user_id,
-      // postId: req.params.id,
     });
 
     res.status(200).json(newComment);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatedComment = await Comment.update(
+      {
+        ...req.body,
+      },
+      { where: { id: req.params.id } }
+    );
+
+    res.status(200).json(updatedComment);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  console.log('Attempted');
+  try {
+    const comment = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        userId: req.session.user_id,
+      },
+    });
+
+    if (!comment) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
